b1f280cb5c9ca762f8972cb7d16777a6ff4dc64a
Gary <gary@smart-itc.com.au>
2019-08-28 09:07:03 +1000

    Chapter 1 Code

--- chapter1/gpio_pkg_check.py
+++ chapter1/gpio_pkg_check.py
@@ -1,1 +1,19 @@
-TODO...
+"""
+  File: chapter1/gpio_pkg_check.py
+  This Python 3 script checks for the availability of various Python GPIO Library Packages for the Raspberry Pi.
+  It does this by attempting to import the Python package. If the package import is successful
+  we report the package as Available, and if the import (or import initialization) fails for any reason,
+  we report the package as Unavailable.
+"""
+try:
+    import gpiozero
+    print('GPIOZero   Available')
+except:
+    print('GPIOZero   Unavailable. Install with "pip install gpiozero"')
+
+try:
+    import pigpio
+    print('PiGPIO    Available')
+except:
+    print('PiGPIO    Unavailable. Install with "pip install pigpio"')
+
