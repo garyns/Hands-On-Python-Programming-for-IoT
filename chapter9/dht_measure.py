7c748849e89bfa4aca9e022f2a8eb646c2c86519
Gary Smart <gary@smart-itc.com.au>
2019-12-05 07:55:36 +1100

    Chapter 8 & 9

---
+++ chapter9/dht_measure.py
@@ -0,0 +1,22 @@
+"""
+File: chapter8/dht_measure.py
+
+Measure temperature and humidity with DHT sensor.
+
+Dependencies:
+  pip3 install pigpio pigpio-dht
+
+Built and tested with Python 3.7 on Raspberry Pi 4 Model B
+"""
+from pigpio_dht import DHT11, DHT22      # (1)
+
+SENSOR_GPIO = 21                         
+sensor = DHT11(SENSOR_GPIO)              # (2)
+#sensor = DHT22(SENSOR_GPIO)
+
+result = sensor.read(retries=2)          # (3)
+print(result)
+
+result = sensor.sample(samples=5)        # (4)
+print(result)
+
