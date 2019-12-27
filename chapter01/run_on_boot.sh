735d63d08a0930cf7724bf5b3e9301b4648e18de
GaryS <gary@smart-itc.com.au>
2019-12-28 10:47:26 +1100

    Updates

--- chapter01/run_on_boot.sh
+++ chapter01/run_on_boot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#File: chapter1/run_on_boot.sh
+#File: chapter01/run_on_boot.sh
 
 # Absolute path to Virtual Environment python interpreter
-PYTHON=/home/pi/pyiot/chapter1/venv/bin/python
+PYTHON=/home/pi/pyiot/chapter01/venv/bin/python
 
 # Absolute path to Python script
-SCRIPT=/home/pi/pyiot/chapter1/gpio_pkg_check.py
+SCRIPT=/home/pi/pyiot/chapter01/gpio_pkg_check.py
 
 # Absolute path to output log file
-LOG=/home/pi/pyiot/chapter1/gpio_pkg_check.log
+LOG=/home/pi/pyiot/chapter01/gpio_pkg_check.log
 
 echo -e "\n####### STARTUP $(date) ######\n" >> $LOG
 
